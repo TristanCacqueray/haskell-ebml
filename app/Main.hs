304cdf517a797caf5abf72e6b61b224a091d8a31
Tristan Cacqueray <tdecacqu@redhat.com>
2023-01-20 13:51:59 +0000

    Add initial tests

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,13 @@
 module Main where
 
+import Codec.EBML qualified as EBML
+import Data.Text.IO qualified
+import System.Environment (getArgs)
+
 main :: IO ()
-main = putStrLn "go!"
+main =
+    getArgs >>= \case
+        [fp] -> do
+            ebml <- EBML.decodeFile EBML.webmSchemas fp
+            Data.Text.IO.putStrLn (EBML.prettyElements ebml)
+        _ -> error "usage: haskell-ebml FILE"
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,5 +1,45 @@
 module Main (main) where
 
+import Codec.EBML.Element (getDataSize)
+import Data.Binary.Get (runGet)
+import Data.Binary.Put (putWord8, runPut)
+import Data.ByteString.Char8 qualified as BS
+import Data.Char (digitToInt)
+import Data.Foldable (traverse_)
+import Data.List (foldl')
+import Data.List.Split (chunksOf)
+import Data.Word (Word64, Word8)
+import Test.Tasty
+import Test.Tasty.Golden
+
 main :: IO ()
-main = do
-    putStrLn "testing!"
+main = defaultMain $ testGroup "Codec.EBML" [unitTests]
+
+unitTests :: TestTree
+unitTests =
+    testGroup
+        "Unit tests"
+        [ goldenVsString "VarInt" "data/var-int.golden"
+            . pure
+            . BS.fromStrict
+            . BS.unlines
+            . map testVarInts
+            $ [ "1000 0010"
+              , "0100 0000 0000 0010"
+              , "0010 0000 0000 0000 0000 0010"
+              , "0001 0000 0000 0000 0000 0000 0000 0010"
+              ]
+        ]
+
+testVarInts :: String -> BS.ByteString
+testVarInts str = BS.pack $ padStr <> " => " <> padVal
+  where
+    padStr = replicate (42 - length str) ' ' <> str
+    padVal = replicate (8 - length val) ' ' <> val
+    val = show $ runGet getDataSize bs
+    bs = runPut $ traverse_ (putWord8 . readOctet) $ chunksOf 8 $ filter (/= ' ') str
+
+readOctet :: String -> Word8
+readOctet s
+    | length s /= 8 = error $ "Invalid length: " <> s
+    | otherwise = fromIntegral $ foldl' (\acc x -> acc * 2 + digitToInt x) 0 s
